9ec26155f881df2771bfee5b1a6fc5226b10ac01
Bhavesh Sarwar <>
2021-11-03 09:54:07 +0530

    - swift example added

---
+++ example_swift/ios/Runner/AppDelegate.swift
@@ -0,0 +1,37 @@
+import UIKit
+import Flutter
+import WebEngage
+import webengage_flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+    var bridge:WebEngagePlugin? = nil
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        
+        // Basic set up
+        bridge = WebEngagePlugin()
+        WebEngage.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions, notificationDelegate: self.bridge)
+        
+        // Push notification delegates
+        WebEngage.sharedInstance().pushNotificationDelegate = self.bridge
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    // Handling universal links
+    override func application(_ application: UIApplication,
+                              continue userActivity: NSUserActivity,
+                              restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool
+    {
+        if let webPageUrl = userActivity.webpageURL{
+            WebEngage.sharedInstance().deeplinkManager.getAndTrackDeeplink(webPageUrl) { location in
+                self.bridge?.trackDeeplinkURLCallback(location)
+            }
+        }
+        return true
+    }
+}
