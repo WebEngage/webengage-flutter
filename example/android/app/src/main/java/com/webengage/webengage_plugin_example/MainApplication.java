e67a19682c9a2b6ba8d54e9ce3bfad606f6d6f6a
Akshay <akshay.chilad@webengage.com>
2023-10-09 21:06:45 +0530

    Removed Android sdk version tracking

--- example/android/app/src/main/java/com/webengage/webengage_plugin_example/MainApplication.java
+++ example/android/app/src/main/java/com/webengage/webengage_plugin_example/MainApplication.java
@@ -30,7 +30,7 @@ public class MainApplication extends FlutterApplication {
         super.onCreate();
         Log.d("webengage", "MainApplicationstarts");
         WebEngageConfig webEngageConfig = new WebEngageConfig.Builder()
-                .setWebEngageKey("14507cbca")
+                .setWebEngageKey("YOUR_LICENSE_CODE")
                 .setAutoGCMRegistrationFlag(false)
                 .setLocationTrackingStrategy(LocationTrackingStrategy.ACCURACY_BEST)
                 .setDebugMode(true) // only in development mode
