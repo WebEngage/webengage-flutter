47ebdd741f91d887c44d6913d4336808a4beb7e5
Milind Keni <milind.keni>
2024-11-13 14:09:03 +0530

    updated functionality of inapp deeplink

--- webengage_flutter_platform_interface/lib/src/utils/we_utils.dart
+++ webengage_flutter_platform_interface/lib/src/utils/we_utils.dart
@@ -1,11 +1,36 @@
+import 'dart:convert';
+
+import 'package:webengage_flutter_platform_interface/src/utils/we_logger.dart';
+
 class WEUtils {
   static String? getInAppDeeplink(
       Map<String, dynamic>? payload, String? actionEId) {
-    if (actionEId == null || payload == null) return null;
+    try {
+      final actions = payload?['actions'];
+      WELogger.i("actions = $actions");
 
-    return (payload['actions'] as List<dynamic>?)?.firstWhere(
-      (action) => action['actionEId'] == actionEId,
-      orElse: () => null,
-    )?['actionLink'];
+      List<dynamic>? actionList;
+
+      if (actions is String) {
+        // Parse actions if it is a JSON string
+        actionList = jsonDecode(actions) as List<dynamic>?;
+      } else if (actions is List<dynamic>) {
+        actionList = actions;
+      }
+
+      if (actionList != null) {
+        return actionList.firstWhere(
+          (action) => action['actionEId'] == actionEId,
+          orElse: () => null,
+        )?['actionLink'];
+      }
+
+      WELogger.e(
+          "getInAppDeeplink: actions is neither a list nor a valid JSON string");
+    } catch (e) {
+      WELogger.e("getInAppDeeplink encountered an error: $e");
+    }
+
+    return null;
   }
 }
