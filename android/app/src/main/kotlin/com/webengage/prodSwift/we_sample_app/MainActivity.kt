23c3ccfc585601c98776544bd058c046604f9006
Milind Keni <milind.keni>
2026-02-23 13:51:06 +0530

    created empty project

--- android/app/src/main/kotlin/com/webengage/prodSwift/we_sample_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.webengage.prodSwift.we_sample_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- ios/NotificationService/NotificationService.swift
+++
@@ -1,7 +0,0 @@
-import UserNotifications
-// Step 1 : Importing WEServiceExtension
-import WEServiceExtension
-
-// Step 2 : Subclassing service Extension
-class NotificationService: WEXPushNotificationService {
-}
